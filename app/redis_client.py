8e42d4588f2b6442d98a0a86bc5e8584d699a055
Lukas Altenkamp <lukas.altenkamp@eon.com>
2025-01-17 23:29:07 +0100

    redis string corrected

--- app/redis_client.py
+++ app/redis_client.py
@@ -3,4 +3,4 @@ import os
 redis_url = os.getenv("REDIS_URL", "redis://localhost:6379")
 
 # Initialize and configure the Redis client connection
-redis_client = redis.Redis.from_url(redis_url, decode_responses=True)
+redis_client = redis.Redis.from_url(redis_url, decode_responses=False)
