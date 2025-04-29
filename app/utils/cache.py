32c79884a9d82bcfeb19ba5bc42d0265f763a7d5
Lukas Altenkamp <lukas.altenkamp@eon.com>
2025-04-29 13:26:47 +0200

    redis URL changed

--- app/utils/cache.py
+++ app/utils/cache.py
@@ -1,10 +1,13 @@
 from flask_caching import Cache
 from app.utils.config import Config
+import os
+
 
 # Initialize the cache
 cache = Cache(
     config={
         "CACHE_TYPE": Config.CACHE_TYPE,
         "CACHE_DEFAULT_TIMEOUT": Config.CACHE_DEFAULT_TIMEOUT,
+        "CACHE_REDIS_URL": os.getenv("REDIS_URL", "redis://localhost:6379/0"),
     }
 )
