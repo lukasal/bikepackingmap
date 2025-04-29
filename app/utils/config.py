0e75b115740c7904c005f95bedaf5bb52edecfff
Lukas Altenkamp <lukas.altenkamp@eon.com>
2025-04-29 13:16:00 +0200

    set SimpleCache to default

--- app/utils/config.py
+++ app/utils/config.py
@@ -8,8 +8,7 @@ class Config:
     SESSION_USE_SIGNER = True
     #
     CACHE_DEFAULT_TIMEOUT = 1800  # Default timeout of 30 minutes
-    USE_REDIS = False  # Set to True to use Redis, False for SimpleCache
-    CACHE_TYPE = os.getenv("CACHE_TYPE", "redis")  # 'redis or 'SimpleCache'
+    CACHE_TYPE = os.getenv("CACHE_TYPE", "SimpleCache")  # 'redis or 'SimpleCache'
     if CACHE_TYPE == "redis":
         CACHE_REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
         SESSION_REDIS = redis_client
